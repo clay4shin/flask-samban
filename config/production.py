80b79a14a19861aab11e6fe3c9db4fbe67f3e82a
Soo Yun Shin <shinsooyun@gmail.com>
2022-08-18 11:38:45 +0900

    config 변경2

--- config/production.py
+++ config/production.py
@@ -2,4 +2,4 @@ from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'samban1.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY =
+SECRET_KEY = b'F\x1cg\x88\xb3\x0b1C\x1e\xd0I>\x93XG\xd3'
