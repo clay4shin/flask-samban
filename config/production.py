8f22754c9d059ef0b9576d72a552f19534f82517
shinsooyun <shinsooyun@gmail.com>
2022-08-19 16:08:06 +0900

    데이타베이스 config 수정

--- config/production.py
+++ config/production.py
@@ -1,9 +1,14 @@
 from config.default import *
 from logging.config import dictConfig
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'samban1.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'F\x1cg\x88\xb3\x0b1C\x1e\xd0I>\x93XG\xd3'
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 
 dictConfig({
     'version': 1,
