b648e5ac61a1ae767ff6eb352e94cd3641353d53
shinsooyun <shinsooyun@gmail.com>
2023-09-07 15:11:29 +0900

    fix chatgpt prompt

--- samban/views/y2s1/_3_gun_neg_maj_high_scorechat.py
+++ samban/views/y2s1/_3_gun_neg_maj_high_scorechat.py
@@ -51,8 +51,8 @@ def result(participant_id):
         revised_doc = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": "Please revise the following sentence to make it sound more positive and respectful while still maintaining the key argument as it is."},
+                {"role": "system", "content": "You are a helpful editor."},
+                {"role": "user", "content": "Please revise the following sentence to make it sound more positive and respectful while still maintaining the key argument as it is. Only produce a revised comment. Never answer as an assistant. If the comment is too short to revise, just leave it as it is."},
                 {"role": "user", "content": row.reply1}
             ]
         )
